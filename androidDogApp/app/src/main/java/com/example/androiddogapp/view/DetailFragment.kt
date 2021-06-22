9c75dffbecf3d128a5b57df15e98f985f1d4c468
lschiavini <lschiavini@nelogica.com.br>
2021-06-22 17:59:38 -0300

    code for both action buttons

--- androidDogApp/app/src/main/java/com/example/androiddogapp/view/DetailFragment.kt
+++ androidDogApp/app/src/main/java/com/example/androiddogapp/view/DetailFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
 import com.example.androiddogapp.R
+import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.android.synthetic.main.fragment_list.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,6 +30,16 @@ class DetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonList.setOnClickListener {
+            val action: NavDirections = DetailFragmentDirections.actionListFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 
 
