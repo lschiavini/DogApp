a75f094f9bb1cc2471d7ae4b3882aec07d6c703e
lschiavini <lschiavini@nelogica.com.br>
2021-06-22 16:09:55 -0300

    changed imports

--- androidDogApp/app/src/main/java/com/example/androiddogapp/view/MainActivity.kt
+++ androidDogApp/app/src/main/java/com/example/androiddogapp/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.androiddogapp
+package com.example.androiddogapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androiddogapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
