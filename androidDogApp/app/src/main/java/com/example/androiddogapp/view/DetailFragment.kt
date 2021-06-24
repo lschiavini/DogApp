f67f4fdd3736ea5f322181f49c7511044f5b478f
lschiavini <lschiavini@nelogica.com.br>
2021-06-24 17:49:59 -0300

    data binding for detail fragment

--- androidDogApp/app/src/main/java/com/example/androiddogapp/view/DetailFragment.kt
+++ androidDogApp/app/src/main/java/com/example/androiddogapp/view/DetailFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavDirections
 import androidx.navigation.Navigation
 import com.example.androiddogapp.R
+import com.example.androiddogapp.databinding.FragmentDetailBinding
+import com.example.androiddogapp.databinding.ItemDogBinding
 import com.example.androiddogapp.model.DogBreed
 import com.example.androiddogapp.util.getProgressDrawable
 import com.example.androiddogapp.util.loadImage
@@ -22,14 +25,18 @@ class DetailFragment : Fragment() {
 
     private lateinit var viewModel: DetailViewModel
 
+    private lateinit var dataBinding: FragmentDetailBinding
+
     private var dogUuid = 0
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
+        return dataBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -37,7 +44,6 @@ class DetailFragment : Fragment() {
         arguments?.let {
             dogUuid = DetailFragmentArgs.fromBundle(it).dogUuid
         }
-
         viewModel = ViewModelProviders.of(this).get(DetailViewModel::class.java)
         viewModel.fetch(dogUuid)
 
@@ -46,15 +52,9 @@ class DetailFragment : Fragment() {
     }
 
     private fun observeViewModel() {
-        viewModel.dogLiveData.observe(viewLifecycleOwner, Observer { dog ->
-            dog?.let {
-                dogName.text = dog.dogBreed
-                dogPurpose.text = dog.bredFor
-                dogTemperament.text = dog.temperament
-                dogLifespan.text = dog.lifeSpan
-                context?.let {
-                    dogImage.loadImage(dog.imageUrl, getProgressDrawable(it))
-                }
+        viewModel.dogLiveData.observe(viewLifecycleOwner, Observer { dogSelected ->
+            dogSelected?.let {
+                dataBinding.dog = dogSelected
             }
         })
 
