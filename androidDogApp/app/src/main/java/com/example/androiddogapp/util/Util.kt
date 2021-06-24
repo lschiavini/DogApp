6775f6bb11f101134d3dee58f94390e0fe0c7642
lschiavini <lschiavini@nelogica.com.br>
2021-06-24 10:56:43 -0300

    extension for imageView

--- androidDogApp/app/src/main/java/com/example/androiddogapp/util/Util.kt
+++ androidDogApp/app/src/main/java/com/example/androiddogapp/util/Util.kt
@@ -1,11 +1,26 @@
 package com.example.androiddogapp.util
 
 import android.content.Context
+import android.widget.ImageView
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.androiddogapp.R
 
 fun getProgressDrawable(context: Context) : CircularProgressDrawable {
     return CircularProgressDrawable(context).apply {
         strokeWidth = 10f
         centerRadius = 50f
+        start()
     }
+}
+
+fun ImageView.loadImage(uri: String?, progressDrawable: CircularProgressDrawable) {
+    val options = RequestOptions()
+        .placeholder(progressDrawable)
+        .error(R.mipmap.ic_dog_icon)
+    Glide.with(context)
+        .setDefaultRequestOptions(options)
+        .load(uri)
+        .into(this)
 }
