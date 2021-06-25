aace6d0f7a33decc0e985956a563e6e78024ca18
lschiavini <lschiavini@nelogica.com.br>
2021-06-25 11:38:14 -0300

    shared preferences gets pref_cache_duration from preferences.xml

--- androidDogApp/app/src/main/java/com/example/androiddogapp/util/SharedPreferencesHelper.kt
+++ androidDogApp/app/src/main/java/com/example/androiddogapp/util/SharedPreferencesHelper.kt
@@ -31,7 +31,9 @@ class SharedPreferencesHelper {
             putLong(PREF_TIME, time)
         }
     }
+
     fun getUpdateTime() = prefs?.getLong(PREF_TIME, 0)
 
+    fun getCacheDuration() = prefs?.getString("pref_cache_duration", "")
 
 }
