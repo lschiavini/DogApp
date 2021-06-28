7b02f6be692642d269de2cd8c57ce2261a6dad2f
lschiavini <lschiavini@nelogica.com.br>
2021-06-28 09:22:04 -0300

    lint

--- androidDogApp/app/src/main/java/com/example/androiddogapp/view/ListFragment.kt
+++ androidDogApp/app/src/main/java/com/example/androiddogapp/view/ListFragment.kt
@@ -25,7 +25,6 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         setHasOptionsMenu(true)
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
